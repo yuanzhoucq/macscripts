f0db7ab3ee4a15eb951e5f123eb6f6ce8c1614e3
yuanzhoucq <yuanzhou.cq@gmail.com>
2018-08-12 12:26:18 +0200

    typo

--- set_volume.py
+++ set_volume.py
@@ -7,7 +7,7 @@ from notify import post_notification
 def set_volume(volume):
     volume = int(volume)
     if volume < 0 or volume > 100:
-        raise ValueError("Volume should worth between 0 and 100.")
+        raise ValueError("Volume should be between 0 and 100.")
     # if the new volume is the same with the old one, we do nothing.
     if volume == int(check_output('osascript -e "output volume of (get volume settings)"', shell=True)):
         return False
